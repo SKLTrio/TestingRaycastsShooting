cb1b9203849d407d9b73e0ebc9dff0150eebc682
Neve Lane <123910307+SKLTrio@users.noreply.github.com>
2023-03-15 01:33:59 +0000

    Week 7 - Still not Implemented

--- Assets/CCTVSystem/Scripts/CCTVMovement.cs
+++ Assets/CCTVSystem/Scripts/CCTVMovement.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class CCTVMovement : MonoBehaviour
 {
+    [SerializeField] private Transform[] transforms;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float minRotation = -30f;
+    [SerializeField] private float maxRotation = 30f;
 
-    [SerializeField]
-    private Transform player;
+    private float currentRotation = 0f;
+    private float direction = 1f;
 
-    [SerializeField]
-    private Transform rotatingCameraPart;
-
-    [SerializeField]
-    float rotationSpeed = 0.5f;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        Rotation();
+        transforms = new Transform[6];
+        for (int i = 0; i < 6; i++)
+        {
+            transforms[i] = transform.GetChild(i);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        foreach (Transform childTransform in transforms)
+        {
+            // Rotate the transform horizontally
+            childTransform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime * direction);
+            currentRotation += rotationSpeed * Time.deltaTime * direction;
+            currentRotation = Mathf.Clamp(currentRotation, minRotation, maxRotation);
+            childTransform.localRotation = Quaternion.Euler(0f, currentRotation, 0f);
 
-    private void Rotation()
-    {
-        var initialrotation = transform.rotation;
-
-        
+            // Change direction if we reach min/max rotation
+            if (currentRotation >= maxRotation || currentRotation <= minRotation)
+            {
+                direction *= -1f;
+            }
+        }
     }
 }
