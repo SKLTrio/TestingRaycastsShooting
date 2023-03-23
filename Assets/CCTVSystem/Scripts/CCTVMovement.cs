5195bbf53812054c2f50882ce70af2672d104a06
Neve Lane <123910307+SKLTrio@users.noreply.github.com>
2023-03-23 00:32:32 +0000

    Week 8 - CCTV Fully Implemented

--- Assets/CCTVSystem/Scripts/CCTVMovement.cs
+++ Assets/CCTVSystem/Scripts/CCTVMovement.cs
@@ -1,60 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.AnimatedValues;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CCTVMovement : MonoBehaviour
 {
-    public float speed = 1f; // speed of the object movement
-    public float angle = 75f; // angle at which the object will be moved
-    public float lightAngle = 150f; // angle at which the object will be moved
-    public float fov = 60f; // field of view in degree
-    public float range = 12f; // the range of the camera.
+    public float Speed = 30f; //Speed of the object movement.
+    public float Angle = 180f; //Angle at which the object will be moved.
+    public float FOV = 120f; //Field of view in degree.
+    public float Range = 12f; //The range of the camera.
+    //public float Light_Blink_Speed = 1f;
 
-    public Transform target; // target to look at (optional)
+    [SerializeField] Transform Player_Obj;
+    [SerializeField] Transform Camera_Parts;
 
-    public Transform CameraPart1;
-    //public Transform CameraPart2;
-    //public Transform CameraLightModel1;
-    //public Transform CameraLightModel2;
-    //public Transform CameraLight;
+    [SerializeField] GameObject Green_Light;
+    [SerializeField] GameObject Red_Light;
+
+    [SerializeField] public AudioSource Alarm;
+
+    bool ShouldRotateCamera = true;
+
+    bool isBeeping = false;
+    bool isRedLightBlinking = false;
+    float timer = 0f;
 
     void Start()
     {
-
+        Green_Light.SetActive(true);
+        Red_Light.SetActive(false);
     }
+
 
     void Update()
     {
-        // calculate the current angle of the object based on time and speed
-        float currentAngle = Mathf.Sin(Time.time * speed * Mathf.Deg2Rad) * 30f;
+        if (ShouldRotateCamera)
+        {
+            float currentAngle = Mathf.Sin(Time.time * Speed * Mathf.Deg2Rad) * FOV / 2;
+            Camera_Parts.localRotation = Quaternion.Euler(Angle, currentAngle, 0f);
+        }
 
-        // set the rotation of the child objects.
-        CameraPart1.localRotation = Quaternion.Euler(angle, currentAngle, 0f);
-        //CameraPart2.localRotation = Quaternion.Euler(angle, currentAngle, 0f);
-        //CameraLightModel1.localRotation = Quaternion.Euler(angle, currentAngle, 0f);
-        //CameraLightModel2.localRotation = Quaternion.Euler(angle, currentAngle, 0f);
-        //CameraLight.localRotation = Quaternion.Euler(lightAngle, currentAngle, 0f);
+        if (!ShouldRotateCamera)
+        {
+            timer += Time.deltaTime;
 
-        //CameraPart1.transform.LookAt(target.position, angle);
+            if (timer >= 2f)
+            {
+                if (!isRedLightBlinking)
+                {
+                    StartCoroutine(BlinkRed_Light());
+                }
+            }
+        }
 
+        else
+        {
+            timer = 0f;
+            isRedLightBlinking = false;
+        }
     }
 
-    
+    private IEnumerator BlinkRed_Light()
+    {
+        isRedLightBlinking = true;
+        while (!ShouldRotateCamera)
+        {
+            Alarm.Play();
+            Red_Light.SetActive(false);
+            yield return new WaitForSecondsRealtime(1f);
+            Red_Light.SetActive(true);
+            yield return new WaitForSecondsRealtime(1f);
+        }
 
-    //private void onTriggerStay(Collider collider)
-    //{
-       // Debug.Log("CCTV Camera Range: " + collider.name);
+        Red_Light.SetActive(false);
+        isRedLightBlinking = false;
+    }
 
-        //if (collider.tag == "Player")
-        //{
-            //Vector3 directionVector = (collider.transform.position - CameraPart1.position);
+    private void OnTriggerStay(Collider collider)
+    {
+        ShouldRotateCamera = false;
+        Green_Light.SetActive(false);
+        Red_Light.SetActive(true);
+        Camera_Parts.transform.LookAt(Player_Obj);
+    }
 
-        //}
+    private void OnTriggerExit(Collider collider)
+    {
+        ShouldRotateCamera = true;
+        Green_Light.SetActive(true);
+        Red_Light.SetActive(false);
+        isRedLightBlinking = false;
+        timer = 0f;
 
-    //}
-
+    }
 
 }
